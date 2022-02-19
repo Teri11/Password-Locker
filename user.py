931f2983ceea08ac08d0926cff9ff2512f5e6522
Teri11 <augustineteri157@gmail.com>
2022-02-19 12:56:06 +0300

    added docstring  on user save

--- user.py
+++ user.py
@@ -10,5 +10,18 @@ def __init__(self,username,password):
   '''
   a method that defines the properties on the class object
   '''
+  
   self.username = username
   self.password = password
+
+  def save_user(self):
+     '''
+        test case to check if the user is added to the user list
+      '''
+  User.user_list.append(self)
+
+  def delete_user(self):
+    '''
+    to check if user details is deleted
+    '''
+    User.user_list.remove(self)
