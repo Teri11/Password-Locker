2f369261c5a7af186be5a27065b6052dc881f283
Teri11 <augustineteri157@gmail.com>
2022-02-19 23:28:11 +0300

    added credentials list

--- credential.py
+++ credential.py
@@ -2,3 +2,10 @@ class Credentials:
   '''
    credential class to assist creation of new instance of credentials
    '''
+
+  credentials_list = [] 
+  
+  def _init_(self,username,password):
+    '''
+    To check if the credentials are initialized properly
+    '''
