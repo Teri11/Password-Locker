da33463cbafcb5fb5df71040dc899de46a6fb68c
Teri11 <augustineteri157@gmail.com>
2022-02-19 12:23:37 +0300

    modified class user

--- user.py
+++ user.py
@@ -2,3 +2,10 @@ class user:
   '''
   class that generates new instances of users
   '''
+  
+
+user_list=[]
+
+def __init__(self,user_name,password):
+  self.user_name = user_name
+  self.password = password
