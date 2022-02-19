9196406c94896e46e126c3b22844455e4eeae8c1
Teri11 <augustineteri157@gmail.com>
2022-02-20 01:12:39 +0300

    added  setup for credentials

--- credential_test.py
+++ credential_test.py
@@ -1,2 +1,10 @@
+# !usr/bin/env python3.9  helps to know which python version you used.
 import unittest #importing unittestb module
 from credentials import credentials
+class TestCredentials(unittest.TestCase):
+     def setUp(self):
+       """
+         a method that defines test cases for the credentials class behaviours.
+        """
+       self.new_credentials = credentials("Augustine","1234")
+        
