323c5baa3cfb4364d4b0651c3165c8d02f86f421
Teri11 <augustineteri157@gmail.com>
2022-02-19 13:37:04 +0300

    added first test

--- user_test.py
+++ user_test.py
@@ -3,25 +3,20 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
-  '''
-  Test class thats defines test cases for the user class behavior
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-  '''
-
-def setUp(self):
-   '''
-   Set up method to run before each test cases.
-   '''
-self.new_user = User("Augustine","1234") # create user object
-
-def test_init(self):
-  '''
-  test_init test case to test if the object is initialized properly
-  '''
-  self.assertEqual(self.new_user.username,"Augustine")
-  self.assertEqual(self.new_user.password,"1234")
+   def setUp(self):
+       """
+         Test class that defines test cases for the user class behaviours.
+        """
+   
+   
+       self.new_user = User("Augustine","1234") # create user object
+   
+   def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,"Augustine")
+        self.assertEqual(self.new_user.password,"1234")
 
 
 if __name__ == '__main__':
