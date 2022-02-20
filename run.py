9804b0c287dbf02fffbee6c9c15fb1f9ee60680b
Teri11 <augustineteri157@gmail.com>
2022-02-20 16:13:51 +0300

    modified log in successful input

--- run.py
+++ run.py
@@ -34,11 +34,11 @@ def main():
   print("Hello,Welcome to Portal Accounts,What is your Name?")
   username =input()
 
-  print(f"Hello {username}.Use the codes below to make your experience better with us")
+  print(f"Hello {username}.\nUse the codes below to make your experience better with us")
   print('\n')
-
+  print('*'*100)
   while True:
-    print("Use the short codes here to access:cu-create new user\ndu-Display new user\nfu-find user\nlogin-log in to your account\nex-exit user list")
+    print("Use the short codes here to access:\ncu-create new user\ndu-Display new user\nfu-find user\nlog-login to your account\nex-exit user list")
     short_code = input().lower()
     if short_code =='cu':
       print("Create New User😀 Account😀")
@@ -47,8 +47,7 @@ def main():
       name = input()
       print("Enter password ...")
       password = input()
-      save_users(create_user(name,password))
-      
+      # save_users(create_user(username,password))
       print('*'*100)
       print(f"Hello {username}\nUser Name:{name}.\nPortal Account has been created successfully.\nPlease proceed to login to access your account")
       print('*'*100)
@@ -56,6 +55,11 @@ def main():
     elif short_code == 'log' or short_code == 'du':
       print('*'*100)
       print("Enter your User Name...")
+      username = input()
+      print("Enter your password ...")
+      password = input()
+      print(f"{name} Logged in to Portal Account successfully")
+      print('*'*100)
      
      
       
