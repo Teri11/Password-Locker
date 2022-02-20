3dceaa8c22b58efbae320a71a28dd7e8b7f260d2
Teri11 <augustineteri157@gmail.com>
2022-02-20 13:16:57 +0300

    added user details

--- run.py
+++ run.py
@@ -1,5 +1,86 @@
 #!/usr/bin/env python3.9
 from user import User
-from credentials import Credentials
-import random
-import string
+from credentials import credentials
+
+#user details
+def create_user(username, password):
+    '''
+    Creating new user
+    '''
+    new_user = (username, password)
+    return new_user
+
+
+def save_user(user):
+    '''
+       To save user details
+    '''
+    user.save_user()
+
+def delete_user(user):
+    '''
+       Function will delete user info
+    '''
+    user.delete_user()
+
+def find_user(user):
+    '''
+    To find user
+    '''
+    user.find_user()
+
+
+def main():
+  print("Hello,Welcome to Portal Accounts,What is your username?")
+  username =input()
+
+  print(f"Hello {username}.What do you want to do in the Portal?")
+  print('\n')
+
+  while True:
+    print("Use the short codes here to access:cu-create new user,du-Display new user,fu-find user,login-log in to your account.ex-exit user list")
+    short_code = input().lower()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    main()
+
+
+
+   
+
