eb0e5761dec50584237ccc62a7f87188767b9076
Teri11 <augustineteri157@gmail.com>
2022-02-20 07:58:20 +0300

    added delete function

--- credential_test.py
+++ credential_test.py
@@ -1,6 +1,6 @@
 # !usr/bin/env python3.9  helps to know which python version you used.
-import unittest  # importing unittestb module
-from credentials import credentials
+import unittest  # Importing the unittest module
+from credential import Credentials
 
 
 class TestCredentials(unittest.TestCase):
@@ -8,14 +8,14 @@ class TestCredentials(unittest.TestCase):
         """
           a method that defines test cases for the credentials class behaviours.
          """
-        self.new_credentials = credentials("Augustine", "1234")
+        self.new_credentials = Credentials("Teri","1234")
 
     def test_init(self):
         '''
          test case to know if credentials has been initialized well
          '''
-        self.assertEqual(self.new_credentials.username, "Augustine")
-        self.assertEqual(self.new_credentials.password, "1234")
+        self.assertEqual(self.new_credentials.username,"Teri")
+        self.assertEqual(self.new_credentials.password,"1234")
 
     def test_save_credentials(self):
         '''
@@ -23,8 +23,23 @@ class TestCredentials(unittest.TestCase):
         '''
         self.new_credentials.test_save_credentials
         self.assertEqual(len(Credentials.credentials_list), 1)
+
     def tearDown(self):
         '''
         A test to clean up after each test case has run.
         '''
         Credentials.credentials_list = []
+   
+    def test_delete_credentials(self):
+      '''
+      Test method to delete our credentials from credentials_list
+      '''
+      self.new_credentials.save_credentials()
+      test_credentials=Credentials("Teri", "1234")
+      test_credential.save_credentials()
+      self.new_credentials.delete_credential()
+      self.assertEqual(len(Credentials.credentials_list),1)
+
+
+if __name__ == '__main__':
+    unittest.main()
