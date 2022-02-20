f676abc117f7ce6397116066f37a4b108722aba0
Teri11 <augustineteri157@gmail.com>
2022-02-20 07:29:54 +0300

    added test initialize for credentials

--- credential_test.py
+++ credential_test.py
@@ -1,10 +1,18 @@
 # !usr/bin/env python3.9  helps to know which python version you used.
-import unittest #importing unittestb module
+import unittest  # importing unittestb module
 from credentials import credentials
+
+
 class TestCredentials(unittest.TestCase):
-     def setUp(self):
-       """
-         a method that defines test cases for the credentials class behaviours.
+    def setUp(self):
         """
-       self.new_credentials = credentials("Augustine","1234")
-        
+          a method that defines test cases for the credentials class behaviours.
+         """
+        self.new_credentials = credentials("Augustine", "1234")
+
+    def test_init(self):
+        '''
+         test case to know if credentials has been initialized well
+         '''
+        self.assertEqual(self.new_credentials.username,"Augustine")
+        self.assertEqual(self.new_credentials.password,"1234")
