55a53c85cee75899373d2a82017eb4cbac4f9b92
Teri11 <augustineteri157@gmail.com>
2022-02-20 12:40:47 +0300

    modified credentials

--- credential_test.py
+++ credential_test.py
@@ -1,6 +1,6 @@
 # !usr/bin/env python3.9  helps to know which python version you used.
 import unittest  # Importing the unittest module
-from credential import Credentials
+from credentials import credentials
 
 
 class TestCredentials(unittest.TestCase):
@@ -8,7 +8,7 @@ class TestCredentials(unittest.TestCase):
         """
           a method that defines test cases for the credentials class behaviours.
          """
-        self.new_credentials = Credentials("Teri", "1234")
+        self.new_credentials = credentials("Teri", "1234")
 
     def test_init(self):
         '''
@@ -17,55 +17,54 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.username, "Teri")
         self.assertEqual(self.new_credentials.password, "1234")
 
-    # def test_save_credentials(self):
-    #     '''
-    #     To test if credentials are saved in credential list
-    #     '''
-    #     self.new_credentials.test_save_credentials
-    #     self.assertEqual(len(Credentials.credentials_list), 1)
+    def test_save_credentials(self):
+        '''
+        To test if credentials are saved in credential list
+        '''
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(credentials.credentials_list),1)
 
-    # def tearDown(self):
-    #     '''
-    #     A test to clean up after each test case has run.
-    #     '''
-    #     Credentials.credentials_list = []
+    def tearDown(self):
+        '''
+        A test to clean up after each test case has run.
+        '''
+        credentials.credentials_list = []
 
-    # def test_delete_credentials(self):
-    #     '''
-    #     Test method to delete our credentials from credentials_list
-    #     '''
-    #     self.new_credentials.save_credentials()
-    #     test_credentials = Credentials("Teri", "1234")
-    #     test_credentials.save_credentials()
-    #     self.new_credentials.delete_credentials()
-    #     self.assertEqual(len(Credentials.credentials_list), 1)
+    def test_delete_credentials(self):
+        '''
+        Test method to delete our credentials from credentials_list
+        '''
+        self.new_credentials.save_credentials()
+        test_credentials = credentials("Teri", "1234")
+        test_credentials.save_credentials()
+        self.new_credentials.delete_credentials()
+        self.assertEqual(len(credentials.credentials_list), 1)
 
-    # def test_display_credentials(self):
-    #     """
-    #     A test to displays all credentials saved by user
-    #     """
-    #     self.assertEqual(Credentials.display_credentials(),
-    #                      credentials.credentials_list)
+    def test_display_credentials(self):
+        """
+        A test to displays all credentials saved by user
+        """
+        self.assertEqual(credentials.display_credentials(),credentials.credentials_list)
 
-    # def test_save_multiple_credentials(self):
-    #     '''
-    #        test_save_multiple_credentials to check if we can save multiple credentials
-    #        objects to our credentials_list
-    #        '''
-    #     self.new_credentials.save_credentials()
-    #     test_credentials = Credentials("Teri", "1234")
-    #     test_credentials.save_credentials()
-    #     self.assertEqual(len(Credentials.credentials_list), 2)
+    def test_save_multiple_credentials(self):
+        '''
+           test_save_multiple_credentials to check if we can save multiple credentials
+           objects to our credentials_list
+           '''
+        self.new_credentials.save_credentials()
+        test_credentials = credentials("Teri", "1234")
+        test_credentials.save_credentials()
+        self.assertEqual(len(credentials.credentials_list), 2)
 
-    # def test_find_credentials(self):
-    #     '''
-    #     find credentials using username and display information
-    #     '''
-    #     self.new_credentials.save_credentials()
-    #     test_credentials = Credentials("Teri", "1234")
-    #     test_credentials.save_credentials()
-    #     found_credentials = Credentials.find_credentials("Mweteri")
-    #     self.assertEqual(found_credentials.username, self.new_credentials.username)
+    def test_find_credentials(self):
+        '''
+        find credentials using username and display information
+        '''
+        self.new_credentials.save_credentials()
+        test_credentials = credentials("Teri", "1234")
+        test_credentials.save_credentials()
+        found_credentials = credentials.find_credentials("username")
+      
 
 
 if __name__ == '__main__':
--- credentials.py
+++ credentials.py
@@ -1,12 +1,11 @@
-class Credentials:
+class credentials:
     '''
      credential class to assist creation of new instance of credentials
      '''
 
     credentials_list = []
-  
 
-    def _init_(self, username, password):
+    def __init__(self, username, password):
         '''
         To check if the credentials are initialized properly
         '''
@@ -17,23 +16,25 @@ class Credentials:
         '''
         test case to save new credentials
         '''
-        Credentials.credentials_list.append(self)
+        credentials.credentials_list.append(self)
 
     def delete_credentials(self):
         '''
         delete_credentials method deletes a saved credential from the list
         '''
-        Credentials.credentials_list.remove(self)
-
-    def display_credentials(self):
+        credentials.credentials_list.remove(self)
+    @classmethod
+    def display_credentials(cls):
         '''
         Display created credentials
         '''
-        Credentials.credentails_list.display(self)
+        
+        return cls.credentials_list
 
 
-@classmethod
-def find_credentials(cls, credentials):
-    for Credentials in cls.credentials_list:
-        if user.credentials == credentials:
-            return credentials
+    @classmethod
+    def find_credentials(cls, username):
+      for user in cls.credentials_list:
+        if user.username == username:
+            return user
+        return False
